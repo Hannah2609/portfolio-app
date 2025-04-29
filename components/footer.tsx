1bcf95c4d47bb080f1804234b97e2899814b7341
Hannah2609 <hann7419@stud.kea.dk>
2025-04-29 12:45:20 +0200

    overflow hidden

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import Links from "./links";
 function Footer() {
   return (
     <AnimatedDiv>
-      <footer className=" relative">
+      <footer className="relative overflow-x-hidden">
         <div className="absolute rounded-full bg-[#ffcc8e] dark:bg-[#4b6cc5] top-30 -right-10 w-1/4 h-1/3 blur-[4rem] md:blur-[6rem] dark:blur-[6rem] dark:md:blur-[10rem] pointer-events-none "></div>
         <div className="absolute rounded-full  bg-[#ffa9de] dark:bg-[#ff6ac6] top-70  right-50  w-1/4 h-1/4 blur-[4rem] md:blur-[6rem] dark:blur-[6rem] dark:md:blur-[8rem] pointer-events-none "></div>
         <div className="relative z-10 flex flex-col items-center justify-center">
